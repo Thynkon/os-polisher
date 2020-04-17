a0057ea9d248746d6cda703a2648370b21d99ef0
Mário Ferreira <marioferreira2110@gmail.com>
2020-04-17 18:10:05 +0200

    Fix awesomewm brightness wiget On my laptop, if I run "xbacklight -get" I get the following error: "No outputs have backlight property". Reference: https://askubuntu.com/a/1060843

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-cat >> "/etc/sddm/Xsetup" <<- EOM
-# Dual monitor setup
-xrandr --output DVI-D-0 --mode 1920x1080  --output HDMI-0 --mode 1920x1080 --primary --left-of DVI-D-0
-EOM
+mode=$1
+
+if [ "${mode}" = "desktop" ]; then
+	cat >> "/etc/sddm/Xsetup" <<- EOM
+	# Dual monitor setup
+	xrandr --output DVI-D-0 --mode 1920x1080  --output HDMI-0 --mode 1920x1080 --primary --left-of DVI-D-0
+	EOM
+else
+	cat >> "/etc/X11/xorg.conf.d/00-brightness.conf" <<- EOM
+	Section "Device"
+		Identifier  "Intel Graphics"
+		Driver      "intel"
+		Option      "Backlight"  "intel_backlight"
+	EndSection
+	EOM
+fi
 
 exit 0
