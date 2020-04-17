adda008f6dda6b908e81b21fdadcdc38a38f0713
Mário Ferreira <marioferreira2110@gmail.com>
2020-04-17 17:46:00 +0200

    Install chezmoi (dotfiles manager)

---
+++ scripts/chezmoi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source "../config.sh"
+source "../lib/lib.sh"
+
+repository="twpayne/chezmoi"
+
+latest_release=$(get_latest_release "$repository")
+latest_release="${latest_release:1}"
+
+dnf install "${GITHUB_HTTPS}/${repository}/releases/download/v${latest_release}/chezmoi-${latest_release}-x86_64.rpm"
